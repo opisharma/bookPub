3ac2052c38713000fdfa2969db2de9e70862cb53
opisharma <opisharma15@gmail.com>
2024-10-17 12:22:41 +0600

    Initial commit with project files

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ApiController extends Controller
+{
+    public function register(Request $request) {
+        $request->validate([
+           "name" => "required|string",
+           "email" => "required|string|email|unique:users",
+           "password" => "required|confirmed"
+        ]);
+
+        User::create([
+
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => bcrypt($request->password)
+        ]);
+
+        return response()->json([
+
+            "status" => true,
+            "message" => "Registration Success",
+            "data" => []
+        ]);
+    }
+    public function login(Request $request) {
+
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        $token = Auth::attempt([
+
+            "email" => $request->email,
+            "password" => $request-> password
+        ]);
+    }
+
+    public function profile() {}
+    public function refreshToken() {}
+    public function logout() {}
+}
---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\Api\ApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::post('register', [ApiController::class, 'register']);
+Route::post('login', [ApiController::class, 'login']);
+
+Route::group([
+    "middleware" => ["auth:api"]
+], function () {
+
+    Route::get('profile', [ApiController::class, 'profile']);
+    Route::get('refreshToken', [ApiController::class, 'refreshToken']);
+    Route::get('logout', [ApiController::class, 'logout']);
+});
